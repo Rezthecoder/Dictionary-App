ec740132486f2ee803e9e5693b07a298bbc19b4e
Suresh <suresh@gmail.com>
2022-12-05 20:16:25 +0900

    all done

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 const express = require('express')
-const axios = require("axios").default;
+const axios = require("axios");
 const app = express()
 
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log(req.params)
-  var options = {
-    method: 'GET',
-    url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/theme/',
-    params: {entry: 'clever'},
-    headers: {
-      'X-RapidAPI-Key': '713065f6a4msha8e579ad9975624p1e2e02jsn582c8a00bbfb',
-      'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
-    }
-  };
+   return res.sendFile(__dirname + "/index.html");
+}
+
+)
+
+app.get('/search', (req, res) => {
+
+//   console.log(req)
+//   var options = {
+//     method: 'GET',
+//     url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/theme/',
+//     params: {entry: req.query.entry},
+//     headers: {
+//       'X-RapidAPI-Key': 'API_KEY'',
+//       'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
+//     }
+//   };
   
 //   axios.request(options).then(function (response) {
+//   res.json(response.data);
 //     console.log(response.data);
-//   }).catch(function (error) {
-//     console.error(error);
+//     }).catch(function (error) {
+//         console.error(error);
 //   });
+ 
 const response={} 
 response.data =
 {
@@ -44,9 +52,11 @@ response.data =
     "result_msg": "Success"
   }
   console.log(response.data)
+    res.json(response.data)
 
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}- http://localhost:3000`)
+  
 })
